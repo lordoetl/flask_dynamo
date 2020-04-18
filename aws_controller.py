3f6271d38d2c224e19d792e611b7feb050626d0d
lordoetl <lordoetl@gmail.com>
2020-04-17 19:30:26 -0700

    Added comments for aws_controller.py

--- aws_controller.py
+++ aws_controller.py
@@ -2,14 +2,22 @@ import boto3
 import time
 
 # dynamo_client = boto3.client('dynamodb')
+#setup a session for boto3 to connect to dynamo.  You need to change the region to the region your Dynamo
+#is in.
 dynamo_client = boto3.Session(region_name='us-east-1').client('dynamodb')
 
-
+#This first function get_all() will return all rows from the table in the table variable.
+#I would certainly make this an argument in future revisions.
+#Only the table variable would need to change to match the table you wish to read.
 def get_all():
     table='bobTable'
     return dynamo_client.scan(
         TableName=table
         )
+
+#This code is here just to initalize your table.  This code will create a DynamoDB named per the variable in the function
+#Again this should probably be made into an argument
+#You only are required to setup the keys for your table
 
 def create_table():
         table='bobTable'
@@ -37,15 +45,19 @@ def create_table():
 	# }
     ]
             , 
+	#The ProvisionedThroughput is how to setup your table to perform properly for your use case.
+	#Increasing those numbers will increase performance (and cost)
             ProvisionedThroughput={
                 'ReadCapacityUnits': 1, 
                 'WriteCapacityUnits': 2
             }
       )
-        time.sleep(5)
-        put_item()
+        time.sleep(5) #this is just here to allow the table to be created before entering data with putItem()
+        put_item() #function to add a single item (this is for the exampe only)
         return response
 
+#put_item() is here to add a single generic row to your table.  I only have this here for demo purposes
+#You could certainly alter this to pass one row of data into the table from your application
 def put_item():
     dynamo_client.put_item(
     TableName='bobTable',
@@ -62,6 +74,8 @@ def put_item():
     }
 )
 
+#This function allows you to retrieve a single item from your table (set in the variable)
+#This would certainly be done with arguments for your tablename and filtered item
 def get_item():
     table='bobTable'
     item='1942Casablanca'
@@ -72,7 +86,8 @@ def get_item():
     TableName=table  #could pass this value in as well to manage multiple tables
     )
     return response
-
+#update_item() is here to allow you to update a single item in the table, note that you would always want to 
+#use your key to indicate which item to update.
 def update_item(item, attribute, value, table): # works, but not going to demo
     dynamo_client.update_item(
     TableName=table, 
@@ -84,14 +99,13 @@ def update_item(item, attribute, value, table): # works, but not going to demo
     }
 )
 
-#calls for testing
+#calls for testing.  These are just here to run test for each item
+
 # put_item()
 # update_item('gaming_nationals_zaf','gamerid','willz9335')
 # response=get_item()
 # bob=get_item('gaming_nationals_zaf')
 # print(bob)
 # print(response)
-# dynamo_client.get_item(TableName='fruitSalad', Key={'fruitName':{'S':'Banana'}})
 
-# dynamo_client.put_item(TableName='fruitSalad', Item={'fruitName':{'S':'Banana'},'key2':{'N':'value2'}})
 
