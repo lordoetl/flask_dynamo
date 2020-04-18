ad34118126a826cf2678753e1dcfd2c54e6e26ff
lordoetl <lordoetl@gmail.com>
2020-04-17 19:41:11 -0700

    Added comments

--- LoadDataToDynamo/loadData.py
+++ LoadDataToDynamo/loadData.py
@@ -2,12 +2,16 @@ import boto3
 import json
 import decimal
 
-# dynamo_client = boto3.Session(region_name='us-east-1').client('dynamodb')
-dynamo_client= boto3.resource('dynamodb', region_name='us-east-1')
+#this script is to help you load a fairly simple JSON file to DynamoDB
+dynamo_client= boto3.resource('dynamodb', region_name='us-east-1')  #establish connection to your dynamoDB
 
-table = dynamo_client.Table('bobTable')
+table = dynamo_client.Table('bobTable') #assign YOUR tablename here
+file="moviedata.json" #full path to your json data
 
-with open("moviedata.json") as json_file:
+#this will loop through your file and put_item each row into your table
+#note taht you have to make sure you are "putting" your key and
+#you have to add the columnnames for each item
+with open(file) as json_file:  
     movies = json.load(json_file, parse_float = decimal.Decimal)
     for movie in movies:
         year = int(movie['year'])
