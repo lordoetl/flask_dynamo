d52754fb95d12b3902d609d27ec76e22b7f948c8
lordoetl <lordoetl@gmail.com>
2020-04-17 19:33:45 -0700

    made index.html more clear

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ item='1942Casablanca'
 
 @app.route('/')
 def index():
-    html=f"""This is the main page.<br> <a href=/dev/get-items> get items</a>
+    html=f"""This is the main page.
+        <br> These links are for demo only.  If you use this as an api, route directly to the link that returns your data
+        <br>  In this case the "get all of the data" link
+        <br>  If you are running this locally remember that you will want to remove /dev from the url.
             <br> <a href=/dev/do_items/>Only do this once</a> 
             <br> <a href=/dev/get-all>get all of the data</a>"""
     return html
